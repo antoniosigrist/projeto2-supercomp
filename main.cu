588d70e4f43cc76856ab02489cdd0ce9b8c711da
antoniosigrist <antoniosigrist@charlo.com.br>
2019-06-05 00:04:32 -0300

    versao provavelemte final

--- main.cu
+++ main.cu
@@ -87,7 +87,7 @@ __global__ void cria_cena(curandState *rand_state,hitable **d_list, hitable **d_
     }
 }
 
-__global__ void render_init(vec3 *fb,int max_x, int max_y,int ns,camera **cam, hitable **world, curandState *rand_state) {
+__global__ void render_init(int max_x, int max_y, curandState *rand_state) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) 
@@ -96,21 +96,6 @@ __global__ void render_init(vec3 *fb,int max_x, int max_y,int ns,camera **cam, h
     
     curand_init(seed, pixel_index, 0, &rand_state[pixel_index]); //repassa o mesmo rand_state para os threads dos blocos utilizados na compilacao para garantir consistencia. 
 
-    curandState local_rand_state = rand_state[pixel_index]; 
-    vec3 col(0,0,0);
-    for(int s=0; s < ns; s++) {
-        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
-        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
-        ray r = (*cam)->get_ray(u, v, &local_rand_state);
-
-        col += color(r, world, &local_rand_state);
-    }
-    rand_state[pixel_index] = local_rand_state;
-    col /= float(ns);
-    col[0] = sqrt(col[0]);
-    col[1] = sqrt(col[1]);
-    col[2] = sqrt(col[2]);
-    fb[pixel_index] = col; //coloca o resultado em fb para ser acessado do host ao final do código
 
 }
 
@@ -135,7 +120,6 @@ __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hit
     col[1] = sqrt(col[1]);
     col[2] = sqrt(col[2]);
     fb[pixel_index] = col; //coloca o resultado em fb para ser acessado do host ao final do código
-
 }
 
 
@@ -201,9 +185,9 @@ int main() {
     dim3 threads(tx,ty);
 
 
-    render_init<<<blocks, threads>>>(fb, nx, ny, ns, d_camera, d_world, d_rand_state); //cria o kernel de tamanho block x threads e inicializa cada thread com o random state gerado pela
+    render_init<<<blocks, threads>>>(nx, ny, d_rand_state); //cria o kernel de tamanho block x threads e inicializa cada thread com o random state gerado pela seed
 
-    //render<<<blocks, threads>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state); // renderiza a imagem no tamanho do bloco e threads estabelicidos, garantindo o mesmo cenário para todas as threads (maior parte do processamento está aqui)
+    render<<<blocks, threads>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state); // renderiza a imagem no tamanho do bloco e threads estabelicidos, garantindo o mesmo cenário para todas as threads (maior parte do processamento está aqui)
 
 
     stop = clock();
