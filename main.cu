4e1faaf2f7d9e5008626f5851560156122c72fe2
antoniosigrist <antoniosigrist@charlo.com.br>
2019-06-04 23:52:38 -0300

    oi

--- main.cu
+++ main.cu
@@ -42,48 +42,10 @@ using namespace std::chrono;
     return vec3(0.0,0.0,0.0); 
 }
 
-__global__ void rand_init(curandState *rand_state) {
+__global__ void cria_cena(curandState *rand_state,hitable **d_list, hitable **d_world, camera **d_camera, int nx, int ny) {
     if (threadIdx.x == 0 && blockIdx.x == 0) {
         curand_init(seed, 0, 0, rand_state); //cria uma seed na thread 0 que vai garantir mesma seed para todas as threads
-    }
-}
 
-__global__ void render_init(int max_x, int max_y, curandState *rand_state) {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
-    if((i >= max_x) || (j >= max_y)) 
-        return;
-    int pixel_index = j*max_x + i;
-    
-    curand_init(seed, pixel_index, 0, &rand_state[pixel_index]); //repassa o mesmo rand_state para os threads dos blocos utilizados na compilacao para garantir consistencia. 
-}
-
-__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state) {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
-    if((i >= max_x) || (j >= max_y)) 
-        return; // garante que nao vai rodar alem dos tamanho definido no kernel
-    int pixel_index = j*max_x + i; // calcula a posicao do pixel no kernel
-    curandState local_rand_state = rand_state[pixel_index]; 
-    vec3 col(0,0,0);
-    for(int s=0; s < ns; s++) {
-        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
-        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
-        ray r = (*cam)->get_ray(u, v, &local_rand_state);
-
-        col += color(r, world, &local_rand_state);
-    }
-    rand_state[pixel_index] = local_rand_state;
-    col /= float(ns);
-    col[0] = sqrt(col[0]);
-    col[1] = sqrt(col[1]);
-    col[2] = sqrt(col[2]);
-    fb[pixel_index] = col; //coloca o resultado em fb para ser acessado do host ao final do código
-}
-
-
-__global__ void create_world(hitable **d_list, hitable **d_world, camera **d_camera, int nx, int ny, curandState *rand_state) {
-    if (threadIdx.x == 0 && blockIdx.x == 0) {
         curandState local_rand_state = *rand_state;
         d_list[0] = new sphere(vec3(0,-1000.0,-1), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
         int i = 1;
@@ -121,8 +83,61 @@ __global__ void create_world(hitable **d_list, hitable **d_world, camera **d_cam
                                  float(nx)/float(ny),
                                  aperture,
                                  dist_to_focus);
+
     }
 }
+
+__global__ void render_init(vec3 *fb,int max_x, int max_y,int ns,camera **cam, hitable **world, curandState *rand_state) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) 
+        return;
+    int pixel_index = j*max_x + i;
+    
+    curand_init(seed, pixel_index, 0, &rand_state[pixel_index]); //repassa o mesmo rand_state para os threads dos blocos utilizados na compilacao para garantir consistencia. 
+
+    curandState local_rand_state = rand_state[pixel_index]; 
+    vec3 col(0,0,0);
+    for(int s=0; s < ns; s++) {
+        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+        ray r = (*cam)->get_ray(u, v, &local_rand_state);
+
+        col += color(r, world, &local_rand_state);
+    }
+    rand_state[pixel_index] = local_rand_state;
+    col /= float(ns);
+    col[0] = sqrt(col[0]);
+    col[1] = sqrt(col[1]);
+    col[2] = sqrt(col[2]);
+    fb[pixel_index] = col; //coloca o resultado em fb para ser acessado do host ao final do código
+
+}
+
+__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) 
+        return; // garante que nao vai rodar alem dos tamanho definido no kernel
+    int pixel_index = j*max_x + i; // calcula a posicao do pixel no kernel
+    curandState local_rand_state = rand_state[pixel_index]; 
+    vec3 col(0,0,0);
+    for(int s=0; s < ns; s++) {
+        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+        ray r = (*cam)->get_ray(u, v, &local_rand_state);
+
+        col += color(r, world, &local_rand_state);
+    }
+    rand_state[pixel_index] = local_rand_state;
+    col /= float(ns);
+    col[0] = sqrt(col[0]);
+    col[1] = sqrt(col[1]);
+    col[2] = sqrt(col[2]);
+    fb[pixel_index] = col; //coloca o resultado em fb para ser acessado do host ao final do código
+
+}
+
 
 __global__ void free_world(hitable **d_list, hitable **d_world, camera **d_camera) { //deleta espaco alocado para cada esfera
     for(int i=0; i < 22*22+1+3; i++) { 
@@ -166,9 +181,6 @@ int main() {
     curandState *d_rand_state2;
     cudaMalloc((void **)&d_rand_state2, 1*sizeof(curandState));
 
-    rand_init<<<1,1>>>(d_rand_state2); // inicializa kernel que cria seed no bloco 0 thread 0
-
-    //cudaDeviceSynchronize();  - Utilizado apenas para debug, não é necessário dado que threads são assincrona entre si, mas sequenciais entre elas
 
     hitable **d_list;
     int num_hitables = 489; //tem que ser maior ou igual a 488 que é o numero de bolinhas criadas :)
@@ -178,19 +190,20 @@ int main() {
     camera **d_camera;
     cudaMalloc((void **)&d_camera, sizeof(camera *));
 
-    create_world<<<1,1>>>(d_list, d_world, d_camera, nx, ny, d_rand_state2); //cria mundo randomico utilizando o estado incial criado por rand_init no bloco 0 thread 0
-
-
     clock_t start, stop;
     start = clock();
+
+
+    cria_cena<<<1,1>>>(d_rand_state2,d_list, d_world, d_camera, nx, ny);  // inicializa kernel que cria seed no bloco 0 thread 0
+
 
     dim3 blocks(nx/tx+1,ny/ty+1); //define o numero de blocos (tx e ty são multiplos de 8 já que a arquitetura de 8x8 threads, garantindo que cada bloco faca um numero pareceido de processamento)
     dim3 threads(tx,ty);
 
-    render_init<<<blocks, threads>>>(nx, ny, d_rand_state); //cria o kernel de tamanho block x threads. 
 
+    render_init<<<blocks, threads>>>(fb, nx, ny, ns, d_camera, d_world, d_rand_state); //cria o kernel de tamanho block x threads e inicializa cada thread com o random state gerado pela
 
-    render<<<blocks, threads>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state); // renderiza a imagem no tamanho do bloco e threads estabelicidos, garantindo o mesmo cenário para todas as threads (maior parte do processamento está aqui)
+    //render<<<blocks, threads>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state); // renderiza a imagem no tamanho do bloco e threads estabelicidos, garantindo o mesmo cenário para todas as threads (maior parte do processamento está aqui)
 
 
     stop = clock();
